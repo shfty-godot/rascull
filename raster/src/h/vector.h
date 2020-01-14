113e7ca13876b2d972d544a77d915fd8e58e2f79
Josh Palmer <jpalmerwatkins@gmail.com>
2020-01-14 03:29:01 +0000

    Further generalize vector types

--- raster/src/h/vector.h
+++ raster/src/h/vector.h
@@ -12,19 +12,12 @@
         return result;                                \
     };
 
-#define VEC3_OP(tn, on, op)                                           \
-    inline tn tn##_##on(const tn lhs, const tn rhs)                   \
-    {                                                                 \
-        tn result = {lhs.x op rhs.x, lhs.y op rhs.y, lhs.z op rhs.z}; \
-        return result;                                                \
-    };
-
-#define VEC4_OP(tn, on, op)                                                           \
-    inline tn tn##_##on(const tn lhs, const tn rhs)                                   \
-    {                                                                                 \
-        tn result = {lhs.x op rhs.x, lhs.y op rhs.y, lhs.z op rhs.z, lhs.w op rhs.w}; \
-        return result;                                                                \
-    };
+#define VEC2_FROM_GVEC2(tn, t)                                                          \
+    inline tn tn##_from_godot_vector2(const godot_vector2 *gv)                          \
+    {                                                                                   \
+        tn result = {(t)api->godot_vector2_get_x(gv), (t)api->godot_vector2_get_y(gv)}; \
+        return result;                                                                  \
+    }
 
 #define VEC2(n, t)     \
     typedef struct n   \
@@ -36,7 +29,26 @@
     VEC2_OP(n, add, +) \
     VEC2_OP(n, sub, -) \
     VEC2_OP(n, mul, *) \
-    VEC2_OP(n, div, /)
+    VEC2_OP(n, div, /) \
+    VEC2_FROM_GVEC2(n, t)
+
+#define VEC3_OP(tn, on, op)                                           \
+    inline tn tn##_##on(const tn lhs, const tn rhs)                   \
+    {                                                                 \
+        tn result = {lhs.x op rhs.x, lhs.y op rhs.y, lhs.z op rhs.z}; \
+        return result;                                                \
+    };
+
+#define VEC3_FROM_GVEC3(tn, t)                                        \
+    inline tn tn##_from_godot_vector3(const godot_vector3 *gv)        \
+    {                                                                 \
+        tn result = {                                                 \
+            (t)api->godot_vector3_get_axis(gv, GODOT_VECTOR3_AXIS_X), \
+            (t)api->godot_vector3_get_axis(gv, GODOT_VECTOR3_AXIS_Y), \
+            (t)api->godot_vector3_get_axis(gv, GODOT_VECTOR3_AXIS_Z), \
+        };                                                            \
+        return result;                                                \
+    }
 
 #define VEC3(n, t)     \
     typedef struct n   \
@@ -49,7 +61,15 @@
     VEC3_OP(n, add, +) \
     VEC3_OP(n, sub, -) \
     VEC3_OP(n, mul, *) \
-    VEC3_OP(n, div, /)
+    VEC3_OP(n, div, /) \
+    VEC3_FROM_GVEC3(n, t)
+
+#define VEC4_OP(tn, on, op)                                                           \
+    inline tn tn##_##on(const tn lhs, const tn rhs)                                   \
+    {                                                                                 \
+        tn result = {lhs.x op rhs.x, lhs.y op rhs.y, lhs.z op rhs.z, lhs.w op rhs.w}; \
+        return result;                                                                \
+    };
 
 #define VEC4(n, t)     \
     typedef struct n   \
@@ -72,11 +92,5 @@ VEC4(ivec4, int)
 VEC2(fvec2, float)
 VEC3(fvec3, float)
 VEC4(fvec4, float)
-
-inline ivec2 ivec2_from_godot_vector2(const godot_vector2 *gv)
-{
-    ivec2 result = {(int)api->godot_vector2_get_x(gv), (int)api->godot_vector2_get_y(gv)};
-    return result;
-}
 
 #endif
