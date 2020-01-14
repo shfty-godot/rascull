c88e1add15bb0285c30f6bd3bbc6faa338847f1c
Josh Palmer <jpalmerwatkins@gmail.com>
2020-01-14 19:20:40 +0000

    Reorganize main scene, add triangle mesh

---
+++ raster/TriangleMesh.gd
@@ -0,0 +1,16 @@
+extends MeshInstance
+tool
+
+# Called when the node enters the scene tree for the first time.
+func _init() -> void:
+	var mesh = ArrayMesh.new()
+	var verts := []
+	verts.resize(ArrayMesh.ARRAY_MAX)
+	verts[ArrayMesh.ARRAY_VERTEX] = [
+		Vector3(-0.5, 0.5, 0.0),
+		Vector3(0.5, 0.5, 0),
+		Vector3(-0.5, -0.5, 0)
+	]
+
+	mesh.add_surface_from_arrays(ArrayMesh.PRIMITIVE_TRIANGLES, verts)
+	set_mesh(mesh)
