6daac8aff25167d4cc92005404d386837cfaeb45
Josh Palmer <jpalmerwatkins@gmail.com>
2020-01-14 02:10:26 +0000

    Simplify camera-relative movement

--- raster/Controller.gd
+++ raster/Controller.gd
@@ -15,9 +15,7 @@ func _physics_process(delta) -> void:
 	wish_vec.z += -1 if Input.is_key_pressed(KEY_E) else 0
 	wish_vec.z += 1 if Input.is_key_pressed(KEY_D) else 0
 
-	translation += global_transform.basis.x * wish_vec.x * delta * 2.0
-	translation += Vector3.UP * wish_vec.y * delta * 2.0
-	translation += global_transform.basis.z * wish_vec.z * delta * 2.0
+	translation += global_transform.basis * wish_vec * delta * 2.0
 
 	rotation = Vector3.ZERO
 	rotate(Vector3.UP, yaw)
@@ -27,4 +25,4 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(2):
 		yaw -= event.relative.x * 0.0015
 		pitch -= event.relative.y * 0.0015
-		pitch = clamp(pitch, deg2rad(-80), deg2rad(80))
+		pitch = clamp(pitch, deg2rad(-89.9), deg2rad(89.9))
