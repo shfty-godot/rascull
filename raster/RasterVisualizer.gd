7f4f8a9d1c4023b12973e8040f619f3349691597
Josh Palmer <jpalmerwatkins@gmail.com>
2020-01-16 03:57:19 +0000

    Overhaul RasterVisualizer to use textures

--- raster/RasterVisualizer.gd
+++ raster/RasterVisualizer.gd
@@ -1,23 +1,42 @@
 extends Control
 
-var depth_buffer = PoolIntArray()
+export(int) var frame_rate = 30 setget set_frame_rate
 
-var skip = 4
-var current = 0
+var timer = Timer.new()
+
+var depth_image = Image.new()
+var depth_texture = ImageTexture.new()
+
+func set_frame_rate(new_frame_rate):
+	if frame_rate != new_frame_rate:
+		frame_rate = new_frame_rate
+		update_timer()
+
+func _ready():
+	timer.connect("timeout", self, "render")
+	add_child(timer)
+	update_timer()
+
+func update_timer():
+	timer.stop()
+	timer.set_wait_time(1.0 / frame_rate)
+	timer.start()
 
 func render():
-	if current == 0:
-		depth_buffer = Raster.get_depth_buffer()
-		update()
+	var far_plane_inv = 1.0 / Raster.get_z_far()
 
-	current = (current + 1) % skip
+	var res  = Raster.get_resolution()
+
+	var depth_buffer = PoolByteArray()
+	for depth in Raster.get_depth_buffer():
+		depth_buffer.append((1.0 - (depth * far_plane_inv)) * 255.0)
+
+	depth_image.create_from_data(res.x, res.y, false, Image.FORMAT_L8, depth_buffer)
+	depth_texture.create_from_image(depth_image);
+	update()
 
 func _draw():
-	if not depth_buffer:
+	if not depth_texture:
 		return
 
-	for x in range(0, 256):
-		for y in range(0, 128):
-			var depth = depth_buffer[x + (y * 256)]
-			var color = Color(depth, depth, depth)
-			draw_rect(Rect2(x, y, 1, 1), color)
+	draw_texture(depth_texture, Vector2.ZERO)
