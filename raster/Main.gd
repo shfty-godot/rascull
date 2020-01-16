3185680371e0c8da24c69b15d0a70e7e79b1dade
Josh Palmer <jpalmerwatkins@gmail.com>
2020-01-16 03:57:32 +0000

    Delete unused script

--- raster/Main.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func rasterize():
-	var start_ts = OS.get_ticks_usec()
-	#Raster.bresenham_line(Vector2.ZERO, Vector2(64, 32))
-	Raster.bresenham_triangle(Vector2(32, 32), Vector2(256 - 32, 128-32), Vector2(32, 128-32))
-	print("Rasterize took " + String(OS.get_ticks_usec() - start_ts) + " usec")
