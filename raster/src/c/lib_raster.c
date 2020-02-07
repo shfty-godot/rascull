4b6c93df4f3f37fb05601c6f0cac7bdf3311f428
Josh Palmer <jpalmerwatkins@gmail.com>
2020-02-07 19:12:49 +0000

    Template-ify lib_raster.c

--- raster/src/c/lib_raster.c
+++ raster/src/c/lib_raster.c
@@ -4,6 +4,13 @@
 
 #include <malloc.h>
 
+#define REGISTER_METHOD(name, function)                                                      \
+    godot_instance_method name = {NULL, NULL, NULL};                                         \
+    name##.method = &function;                                                               \
+                                                                                             \
+    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", #name, \
+                                                         attributes, name);
+
 void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *p_options)
 {
     api = p_options->api_struct;
@@ -47,129 +54,23 @@ void GDN_EXPORT godot_nativescript_init(void *p_handle)
     // Register methods
     godot_method_attributes attributes = {GODOT_METHOD_RPC_MODE_DISABLED};
 
-    // get_resolution
-    godot_instance_method get_resolution = {NULL, NULL, NULL};
-    get_resolution.method = &raster_get_resolution;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "get_resolution",
-                                                         attributes, get_resolution);
-
-    // get_z_near
-    godot_instance_method get_z_near = {NULL, NULL, NULL};
-    get_z_near.method = &raster_get_z_near;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "get_z_near",
-                                                         attributes, get_z_near);
-
-    // get_z_far
-    godot_instance_method get_z_far = {NULL, NULL, NULL};
-    get_z_far.method = &raster_get_z_far;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "get_z_far",
-                                                         attributes, get_z_far);
-
-    // set_flip_fov
-    godot_instance_method set_flip_fov = {NULL, NULL, NULL};
-    set_flip_fov.method = &raster_set_flip_fov;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "set_flip_fov",
-                                                         attributes, set_flip_fov);
-
-    // set_fov
-    godot_instance_method set_resolution = {NULL, NULL, NULL};
-    set_resolution.method = &raster_set_resolution;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "set_resolution",
-                                                         attributes, set_resolution);
-
-    // set_fov
-    godot_instance_method set_fov = {NULL, NULL, NULL};
-    set_fov.method = &raster_set_fov;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "set_fov",
-                                                         attributes, set_fov);
-
-    // set_aspect
-    godot_instance_method set_aspect = {NULL, NULL, NULL};
-    set_aspect.method = &raster_set_aspect;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "set_aspect",
-                                                         attributes, set_aspect);
-
-    // set_z_near
-    godot_instance_method set_z_near = {NULL, NULL, NULL};
-    set_z_near.method = &raster_set_z_near;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "set_z_near",
-                                                         attributes, set_z_near);
-
-    // set_z_far
-    godot_instance_method set_z_far = {NULL, NULL, NULL};
-    set_z_far.method = &raster_set_z_far;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "set_z_far",
-                                                         attributes, set_z_far);
-
-    // get_depth
-    godot_instance_method get_depth = {NULL, NULL, NULL};
-    get_depth.method = &raster_get_depth;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "get_depth",
-                                                         attributes, get_depth);
-
-    // get_depth_buffer
-    godot_instance_method get_depth_buffer = {NULL, NULL, NULL};
-    get_depth_buffer.method = &raster_get_depth_buffer;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "get_depth_buffer",
-                                                         attributes, get_depth_buffer);
-
-    // clear_depth_buffer
-    godot_instance_method clear_depth_buffer = {NULL, NULL, NULL};
-    clear_depth_buffer.method = &raster_clear_depth_buffer;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "clear_depth_buffer",
-                                                         attributes, clear_depth_buffer);
-
-    // bresenham_line
-    godot_instance_method bresenham_line = {NULL, NULL, NULL};
-    bresenham_line.method = &raster_bresenham_line;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "bresenham_line",
-                                                         attributes, bresenham_line);
-
-    // bresenham_triangle
-    godot_instance_method bresenham_triangle = {NULL, NULL, NULL};
-    bresenham_triangle.method = &raster_bresenham_triangle;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "bresenham_triangle",
-                                                         attributes, bresenham_triangle);
-
-    // bresenham_triangles
-    godot_instance_method bresenham_triangles = {NULL, NULL, NULL};
-    bresenham_triangles.method = &raster_bresenham_triangles;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "bresenham_triangles",
-                                                         attributes, bresenham_triangles);
-
-    // rasterize_triangles
-    godot_instance_method rasterize_triangles = {NULL, NULL, NULL};
-    rasterize_triangles.method = &raster_rasterize_triangles;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "rasterize_triangles",
-                                                         attributes, rasterize_triangles);
-
-    // rasterize_objects
-    godot_instance_method rasterize_objects = {NULL, NULL, NULL};
-    rasterize_objects.method = &raster_rasterize_objects;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "rasterize_objects",
-                                                         attributes, rasterize_objects);
-
-    // depth_test
-    godot_instance_method depth_test = {NULL, NULL, NULL};
-    depth_test.method = &raster_depth_test;
-
-    nativescript_api->godot_nativescript_register_method(p_handle, "Raster", "depth_test",
-                                                         attributes, depth_test);
+    REGISTER_METHOD(get_resolution, raster_get_resolution)
+    REGISTER_METHOD(get_z_near, raster_get_z_near)
+    REGISTER_METHOD(get_z_far, raster_get_z_far)
+    REGISTER_METHOD(set_flip_fov, raster_set_flip_fov)
+    REGISTER_METHOD(set_resolution, raster_set_resolution)
+    REGISTER_METHOD(set_fov, raster_set_fov)
+    REGISTER_METHOD(set_aspect, raster_set_aspect)
+    REGISTER_METHOD(set_z_near, raster_set_z_near)
+    REGISTER_METHOD(set_z_far, raster_set_z_far)
+    REGISTER_METHOD(get_depth, raster_get_depth)
+    REGISTER_METHOD(get_depth_buffer, raster_get_depth_buffer)
+    REGISTER_METHOD(clear_depth_buffer, raster_clear_depth_buffer)
+    REGISTER_METHOD(bresenham_line, raster_bresenham_line)
+    REGISTER_METHOD(bresenham_triangle, raster_bresenham_triangle)
+    REGISTER_METHOD(bresenham_triangles, raster_bresenham_triangles)
+    REGISTER_METHOD(rasterize_triangles, raster_rasterize_triangles)
+    REGISTER_METHOD(rasterize_objects, raster_rasterize_objects)
+    REGISTER_METHOD(depth_test, raster_depth_test)
+    REGISTER_METHOD(simd_test, raster_simd_test)
 }
