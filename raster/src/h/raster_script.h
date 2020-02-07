072167ac4a8da3792f884009fdd066d4790af3bf
Josh Palmer <jpalmerwatkins@gmail.com>
2020-02-07 23:39:08 +0000

    Template-ify raster_script header

--- raster/src/h/raster_script.h
+++ raster/src/h/raster_script.h
@@ -3,64 +3,33 @@
 
 #include <gdnative_api_struct.gen.h>
 
+#define GODOT_DECLARE_FUNCTION(name)                                                   \
+    godot_variant name(godot_object *p_instance, void *p_method_data, \
+                                        void *p_user_data, int p_num_args, godot_variant **p_args)
+
 void *raster_constructor(godot_object *p_instance, void *p_method_data);
 void raster_destructor(godot_object *p_instance, void *p_method_data, void *p_user_data);
 
-godot_variant raster_get_resolution(godot_object *p_instance, void *p_method_data,
-                                    void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_get_z_near(godot_object *p_instance, void *p_method_data,
-                                void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_get_z_far(godot_object *p_instance, void *p_method_data,
-                               void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_set_resolution(godot_object *p_instance, void *p_method_data,
-                                    void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_set_flip_fov(godot_object *p_instance, void *p_method_data,
-                                  void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_set_fov(godot_object *p_instance, void *p_method_data,
-                             void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_set_aspect(godot_object *p_instance, void *p_method_data,
-                                void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_set_z_near(godot_object *p_instance, void *p_method_data,
-                                void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_set_z_far(godot_object *p_instance, void *p_method_data,
-                               void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_get_depth(godot_object *p_instance, void *p_method_data,
-                               void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_get_depth_buffer(godot_object *p_instance, void *p_method_data,
-                                      void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_clear_depth_buffer(godot_object *p_instance, void *p_method_data,
-                                        void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_bresenham_line(godot_object *p_instance, void *p_method_data,
-                                    void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_bresenham_triangle(godot_object *p_instance, void *p_method_data,
-                                        void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_bresenham_triangles(godot_object *p_instance, void *p_method_data,
-                                         void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_rasterize_triangles(godot_object *p_instance, void *p_method_data,
-                                         void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_rasterize_objects(godot_object *p_instance, void *p_method_data,
-                                       void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_depth_test(godot_object *p_instance, void *p_method_data,
-                                void *p_user_data, int p_num_args, godot_variant **p_args);
-
-godot_variant raster_simd_test(godot_object *p_instance, void *p_method_data,
-                               void *p_user_data, int p_num_args, godot_variant **p_args);
+GODOT_DECLARE_FUNCTION(raster_get_resolution);
+GODOT_DECLARE_FUNCTION(raster_get_z_near);
+GODOT_DECLARE_FUNCTION(raster_get_z_far);
+GODOT_DECLARE_FUNCTION(raster_set_resolution);
+GODOT_DECLARE_FUNCTION(raster_set_flip_fov);
+GODOT_DECLARE_FUNCTION(raster_set_fov);
+GODOT_DECLARE_FUNCTION(raster_set_aspect);
+GODOT_DECLARE_FUNCTION(raster_set_z_near);
+GODOT_DECLARE_FUNCTION(raster_set_z_far);
+GODOT_DECLARE_FUNCTION(raster_set_view_matrices);
+GODOT_DECLARE_FUNCTION(raster_get_depth);
+GODOT_DECLARE_FUNCTION(raster_get_depth_buffer);
+GODOT_DECLARE_FUNCTION(raster_clear_depth_buffer);
+GODOT_DECLARE_FUNCTION(raster_bresenham_line);
+GODOT_DECLARE_FUNCTION(raster_bresenham_triangle);
+GODOT_DECLARE_FUNCTION(raster_bresenham_triangles);
+GODOT_DECLARE_FUNCTION(raster_rasterize_triangles);
+GODOT_DECLARE_FUNCTION(raster_rasterize_objects);
+GODOT_DECLARE_FUNCTION(raster_depth_test);
+GODOT_DECLARE_FUNCTION(raster_depth_test_objects);
+GODOT_DECLARE_FUNCTION(raster_simd_test);
 
 #endif
